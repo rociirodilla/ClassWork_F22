8ced64dde96020b95766a9a97e4c9cec2ed2b812
rociirodilla <rocii.rodilla@gmail.com>
2022-09-05 12:46:33 -0400

    Added a function that lets the user input an HDL value

--- blood_calculator.py
+++ blood_calculator.py
@@ -7,5 +7,7 @@ def interface():
         choice = input("Enter your choice: ")
         if choice=='9':
             return
-   
+def HDL_value():
+    HDL_input=input("Enter HDL value for the patient:")
+    return int(HDL_input)
 interface()
