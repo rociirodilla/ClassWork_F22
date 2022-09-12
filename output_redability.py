08c77116149e8cd439336e3b6af707a0f12e3567
rociirodilla <rocii.rodilla@gmail.com>
2022-09-12 12:16:12 -0400

    This is developer #1

--- output_redability.py
+++ output_redability.py
@@ -3,4 +3,4 @@ dosage = weight * 30
 
 print("CORRECT DOSAGE")
 print("For a patient weighing {:.1f} kg,".format(weight))
-print("  the correct dosage is {} mg the first day".format(dosage))
+print("  the correct dosage is {:,1f} mg the first day".format(dosage))
