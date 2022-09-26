8cc64b8fde9bbe73397fe90725651d33c696ddba
rociirodilla <rocii.rodilla@gmail.com>
2022-09-26 13:58:34 -0400

    Added class to make the definition of the patient information easier

--- database.py
+++ database.py
@@ -1,25 +1,42 @@
-def create_patient_entry(patient_first, patient_last, patient_id, patient_age):
-    new_patient = {"First Name":patient_first,
-                   "Last Name": patient_last,
-                   "ID":patient_id,
-                   "Age": patient_age,
-                   "Test": []}
+class Patient:
+
+    def __init__(self, patient_first, patient_last,
+                 patient_id, patient_age):
+        self.first_name = patient_first
+        self.last_name = patient_last
+        self.patient_id = patient_id
+        self.age = patient_age
+        self.test = []
+
+    def full_name(self):
+        return "{} {}".format(self.first_name,
+                               self.last_name)
+
+
+def create_patient_entry(patient_first, patient_last,
+                         patient_id, patient_age):
+    new_patient = Patient(patient_first, patient_last,
+                          patient_id, patient_age)
+    
     return new_patient
 
 
 def full_name(patient):
-    full_name= "{} {}".format(patient["First Name"], patient["Last Name"])
+    full_name = "{} {}".format(patient["First Name"],
+                               patient["Last Name"])
     return full_name
     
     
 def printing_params(db):
     for patient in db:
         print(patient)
-        print("Name:{},ID :{},Age{}".format(full_name(db[patient]),db[patient]["ID"], db[patient]["Age"]))
+        print("Name:{},ID :{},Age{}".format(full_name(db[patient]),
+                                            db[patient]["ID"],
+                                            db[patient]["Age"]))
 
 
 def find_patient(db, id_num):
-    patient=db[id_num]
+    patient = db[id_num]
     return patient
 
 def test_results(db, id, test_name, test_value):
@@ -36,14 +53,21 @@ def adult_or_minor(patient):
         return "minor"
 
 def main():
-    db = {}
-    db[11]=create_patient_entry("Ann", "Ables", 11, 30)
-    db[22]=create_patient_entry("Bob", "Boyles", 22, 34)
-    db[3]=create_patient_entry("Chris", "Chou", 3, 25)
+    x = Patient("Rocio","Rodriguez","","")
+    print(x.full_name())
+    
+    y=Patient("David","Ward","","")
+    print(y.full_name())
+    exit()
+    
+    #db = {}
+    #db[11] = create_patient_entry("Ann", "Ables", 11, 30)
+    #db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
+    #db[3] = create_patient_entry("Chris", "Chou", 3, 25)
     #print(db)
-    printing_params(db)
-    test_results(db, 3, "HDL", 100)
-    printing_params(db)
+    #printing_params(db)
+    #test_results(db, 3, "HDL", 100)
+    #printing_params(db)
     #print("Patient {} is a {}".format(full_name(db[2]),adult_or_minor(db[2])))
    
    
