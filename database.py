7e02168c0bcce1fdd8f328ff1003a00096ad29a6
rociirodilla <rocii.rodilla@gmail.com>
2022-09-28 13:03:38 -0400

    Modified to adhere to PEP-8

--- database.py
+++ database.py
@@ -10,14 +10,13 @@ class Patient:
 
     def full_name(self):
         return "{} {}".format(self.first_name,
-                               self.last_name)
+                              self.last_name)
 
 
 def create_patient_entry(patient_first, patient_last,
                          patient_id, patient_age):
     new_patient = Patient(patient_first, patient_last,
                           patient_id, patient_age)
-    
     return new_patient
 
 
@@ -25,8 +24,8 @@ def full_name(patient):
     full_name = "{} {}".format(patient["First Name"],
                                patient["Last Name"])
     return full_name
-    
-    
+
+
 def printing_params(db):
     for patient in db:
         print(patient)
@@ -38,6 +37,7 @@ def printing_params(db):
 def find_patient(db, id_num):
     patient = db[id_num]
     return patient
+
 
 def test_results(db, id, test_name, test_value):
     test = [test_name, test_value]
@@ -52,24 +52,24 @@ def adult_or_minor(patient):
     else:
         return "minor"
 
+
 def main():
-    x = Patient("Rocio","Rodriguez","","")
+    x = Patient("Rocio", "Rodriguez", "", "")
     print(x.full_name())
-    
-    y=Patient("David","Ward","","")
+    y = Patient("David", "Ward", "", "")
     print(y.full_name())
     exit()
-    
-    #db = {}
-    #db[11] = create_patient_entry("Ann", "Ables", 11, 30)
-    #db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
-    #db[3] = create_patient_entry("Chris", "Chou", 3, 25)
-    #print(db)
-    #printing_params(db)
-    #test_results(db, 3, "HDL", 100)
-    #printing_params(db)
-    #print("Patient {} is a {}".format(full_name(db[2]),adult_or_minor(db[2])))
-   
-   
+    # db = {}
+    # db[11] = create_patient_entry("Ann", "Ables", 11, 30)
+    # db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
+    # db[3] = create_patient_entry("Chris", "Chou", 3, 25)
+    # print(db)
+    # printing_params(db)
+    # test_results(db, 3, "HDL", 100)
+    # printing_params(db)
+    # print("Patient {} is a {}".format(full_name(db[2]),
+    # adult_or_minor(db[2])))
+
+
 if __name__ == "__main__":
     main()
